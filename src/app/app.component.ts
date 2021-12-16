372297d6fa0e55f7fa2df7c8405979bef22fd4b0
JulioVladimirOreQuispe <1672506605@undc.edu.pe>
2021-12-16 12:12:41 -0500

    modificando arreglos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,20 +18,20 @@ export class AppComponent {
   constructor(){
     this.arrayComida=[
       new Producto("Ceviche","https://www.aboutespanol.com/thmb/G_Ps3MJ7KlSfWNqKAP0yeXlfMZc=/3776x2520/filters:fill(auto,1)/ceviche-5b01daff6bf0690036a376d8.jpg",15),
-      new Producto("Arroz con Pollo","https://comidasperuanas.net/wp-content/uploads/2015/07/Arroz-con-pollo-peruano.jpg",10),
-      new Producto("Chaufa de Pollo","https://cdn.bolivia.com/gastronomia/2012/11/13/arroz-chaufa-con-pollo-3497.jpg",18),
-      new Producto("Ceviche","https://www.aboutespanol.com/thmb/G_Ps3MJ7KlSfWNqKAP0yeXlfMZc=/3776x2520/filters:fill(auto,1)/ceviche-5b01daff6bf0690036a376d8.jpg",15),
-      new Producto("Arroz con Pollo","https://comidasperuanas.net/wp-content/uploads/2015/07/Arroz-con-pollo-peruano.jpg",10),
-      new Producto("Chaufa de Pollo","https://cdn.bolivia.com/gastronomia/2012/11/13/arroz-chaufa-con-pollo-3497.jpg",18),
+      new Producto("Arroz con pollo","https://comidasperuanas.net/wp-content/uploads/2015/07/Arroz-con-pollo-peruano.jpg",10),
+      new Producto("Chaufa de pollo","https://cdn.bolivia.com/gastronomia/2012/11/13/arroz-chaufa-con-pollo-3497.jpg",18),
+      new Producto("Chihcarron de pescado","https://i2.wp.com/www.berypez.pe/wp-content/uploads/2020/06/p04-chicharron-de-pescado.jpg",25),
+      new Producto("Chita frita","https://i.pinimg.com/originals/98/a8/3c/98a83cb0b7ba62f70edb7c304d984e1b.jpg",10),
+      new Producto("Chupe de camarones","https://img-global.cpcdn.com/recipes/1478510_169f43c3ceb34d13/1200x630cq70/photo.jpg",18),
     ];
 
     this.arrayBebida=[
-      new Producto("Inka Cola","https://merkaplace.ca/2774-large_default/botella-de-inca-kola-peruana-300ml-original.jpg",2.5),
-      new Producto("Fanta","https://www.burgerking.pe/Multimedia/productos/facebook/FANTA_ORIGINAL_V2.png",3.5),
+      new Producto("Inka Cola","https://merkaplace.ca/2774-large_default/botella-de-inca-kola-peruana-300ml-original.jpg",2.50),
+      new Producto("Fanta","https://www.burgerking.pe/Multimedia/productos/facebook/FANTA_ORIGINAL_V2.png",3.50),
       new Producto("Coca Cola","https://tatami.com.pe/159-large_default/coca-cola.jpg",18),
-      new Producto("Inka Cola","https://merkaplace.ca/2774-large_default/botella-de-inca-kola-peruana-300ml-original.jpg",2.5),
-      new Producto("Fanta","https://www.burgerking.pe/Multimedia/productos/facebook/FANTA_ORIGINAL_V2.png",3.5),
-      new Producto("Coca Cola","https://tatami.com.pe/159-large_default/coca-cola.jpg",18)
+      new Producto("Pepsi","https://wongfood.vteximg.com.br/arquivos/ids/273848-750-750/103181-1.jpg",2.20),
+      new Producto("Guarana","http://s3.amazonaws.com/imagenes-sellers-mercado-ripley/2020/11/11100735/6304.png",3.20),
+      new Producto("Sprite","https://tatami.com.pe/191-large_default/sprite.jpg",2.50)
     ];
     this.arraySeleccionados=[];
     
